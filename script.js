36dc88313ac53c6fb5bd1b22947ebc0cbe793483
Aguycodes <aguycodes25@gmail.com>
2023-07-03 12:10:56 +0100

    finished making changes to the homepage

--- script.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.headnav ul');
-
-burger.addEventListener('click', () => {
-  nav.classList.toggle('link-active');
-  burger.classList.toggle('toggle');
-});
