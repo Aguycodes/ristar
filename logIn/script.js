d60672f18614e148dc05a3bc6929793669db622f
Aguycodes <aguycodes25@gmail.com>
2023-07-03 14:34:21 +0100

    reedited the login page

--- logIn/script.js
+++ logIn/script.js
@@ -1,8 +1,22 @@
 const showPass = () => {
-    let x = document.getElementById('passw');
+    let x = document.getElementById('floatingPassword');
     if(x.type === 'password'){
         x.type = 'text';
     } else {
         x.type = 'password';
     }
 }
+
+const input1 = document.getElementById('floatingInput');
+const input2 = document.getElementById('floatingPassword')
+const submitbtn = document.getElementById('link');
+
+input1.addEventListener('input', checkFormCompletion);
+input2.addEventListener('input', checkFormCompletion);
+
+function checkFormCompletion() {
+  if (input1.value !== '' && input2.value !== '') {
+    submitbtn.classList.remove('disabled');
+  }
+}
+
