414a17fa662effb97e429ca8ea61bd063bb9fb3b
Aguycodes <aguycodes25@gmail.com>
2023-07-04 23:58:40 +0100

    made changes to register page

--- signUp/script.js
+++ signUp/script.js
@@ -1,8 +1,29 @@
+//show password
 const showPass = () => {
-    let x = document.getElementById('passw');
+    let x = document.getElementById('floatingPassword');
     if(x.type === 'password'){
         x.type = 'text';
     } else {
         x.type = 'password';
     }
+}
+
+//form validation
+const input1 = document.getElementById('floatingInput');
+const input2 = document.getElementById('floatingEmail');
+const input3 = document.getElementById('floatingPassword');
+const input4 = document.getElementById('check');
+const submitbtn = document.getElementById('link');
+
+input1.addEventListener('input', checkFormCompletion);
+input2.addEventListener('input', checkFormCompletion);
+input3.addEventListener('input', checkFormCompletion);
+input4.addEventListener('change', checkFormCompletion);
+
+function checkFormCompletion() {
+  if (input1.value !== '' && input2.value !== '' && input3.value !== '' && input4 === 'checked') {
+    submitbtn.classList.remove('disabled');
+  } else {
+    submitbtn.classList.add('disabled');
+  }
 }
