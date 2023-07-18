8cddcc19ebf58ae678d7b4b900b0daf059592022
Aguycodes <aguycodes25@gmail.com>
2023-07-18 19:42:08 +0100

    added the api

--- account/services/airtime.js
+++ account/services/airtime.js
@@ -1,10 +1,24 @@
-function submit(){
-    let networkN = document.getElementById("netw");
-    const user = {
-        token: "tlnzyh7jsmfcxdbiviuehvobta52dgxu9w8mfkok0szpwercar",
-        network: networkN.value,
-        phone: document.getElementById("phoneNo").value,
-        amount: document.getElementById("amt").value
-    }
-    console.log(user);
+const token = 'tlnzyh7jsmfcxdbiviuehvobta52dgxu9w8mfkok0szpwercar';
+const network = document.getElementById("netw").value;
+const phone = document.getElementById("phoneNo").value;
+const amount = document.getElementById("amt").value;
+
+function submitOrder(){
+fetch('http://datagifting.com.ng/api/airtime.php?token=' + token + '&network=' + network + '&phone_number=' + phone + '&amount=' + amount)
+.then(function(response) {
+  if (response.ok) {
+    // Request was successful
+    return response.text();
+  } else {
+    // Request failed
+    throw new Error('Error: ' + response.status);
+  }
+})
+.then(function(data) {
+  window.alert(data);
+})
+.catch(function(error) {
+  window.alert(error);
+});
+
 }
